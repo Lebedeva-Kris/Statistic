d4526b722aa999d177a4ee14f8c553816b25eddd
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-25 20:39:30 +0300

    Test code is added

---
+++ src/test/java/StatisticServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticServiceTest {
+
+    @Test
+    void findMax(){
+        StatisticService service = new StatisticService();
+
+                long[] incomesInBillions = {1, 2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+}
